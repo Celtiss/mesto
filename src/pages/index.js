4bf3dbeaf58a879fa9bbb32c3a2db02e01d5d4e7
Elena Temirbekova <temn.lena@mail.ru>
2022-09-05 20:59:50 +0300

    feat:Собрать проект с помощью webpack

---
+++ src/pages/index.js
@@ -0,0 +1,122 @@
+import './index.css';
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { Section } from '../components/Section.js';
+import { Popup } from '../components/Popup.js';
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { UserInfo } from '../components/UserInfo.js';
+import { formSettings, initialCards, selectors } from '../utils/constants.js';
+
+//Редактирование профиля
+const popupEditProfile = document.querySelector(selectors.popupEdit);
+const popupOpenButtonEditProfile = document.querySelector(selectors.editButton);
+
+//Карточки
+const popupCards = document.querySelector(selectors.popupCards);
+const popupOpenButtonAddCards = document.querySelector(selectors.addCardsButton);
+const cardsSection = document.querySelector(selectors.cardsSection);
+
+//Попап с картинкой
+const popupImage = document.querySelector(selectors.popupImage);
+
+//Form
+const formCard = document.querySelector(selectors.popupFormAddCard);
+
+//Профиль
+const profile = document.querySelector(selectors.profileInfo);
+const profileName = profile.querySelector(selectors.profileName);
+const profileJob = profile.querySelector(selectors.profileDescription);
+
+const profileForm = popupEditProfile.querySelector(selectors.popupForm);
+const nameInput = profileForm.querySelector(selectors.popupInputName);
+const jobInput = profileForm.querySelector(selectors.popupInputJob);
+
+
+// Заполнение popup_profile
+const fillPopupEdit = function () {
+    const userData = userProfie.getUserInfo();
+    nameInput.value = userData.name;
+    jobInput.value = userData.info;
+    createPopupExemplar(popupEditProfile).open();
+};
+
+//Создание экземпляра класса popup
+const createPopupExemplar = function (popupItem) {
+    const popup = new Popup (popupItem);
+    popup.setEventListeners();
+    return popup;
+};
+
+const createPopupImageExemplar = function () {
+    const bigImagePopup = new PopupWithImage(popupImage);
+    bigImagePopup.setEventListeners();
+    return bigImagePopup;
+}
+
+const userProfie = new UserInfo ({userName:profileName, userInfo:profileJob});
+
+const popupFormProfile = new PopupWithForm ({
+    popupSelector: popupEditProfile,
+    handleSubmitForm: (formData) => {
+        userProfie.setUserInfo(formData);
+        createPopupExemplar(popupEditProfile).close();
+    }
+});
+
+popupFormProfile.setEventListeners();
+
+//Создание экземпляра карточки
+function createCard (title, img, cardTemplate) {
+    const card = new Card(title, img, cardTemplate, createPopupImageExemplar().open); 
+    const cardElement = card.generateCard();
+    return cardElement;
+}
+
+const popupFormCard = new PopupWithForm ({
+    popupSelector: popupCards,
+    handleSubmitForm: (formData) => {
+        const cardElement = createCard(formData.popupName,formData.popupImg, selectors.cardTemplate);
+        cardList.setElement(cardElement);
+        createPopupExemplar(popupCards).close();
+    }
+});
+popupFormCard.setEventListeners();
+
+//Инициализация 6 начальных карточек
+const cardList = new Section({
+    items: initialCards,
+    renderer: (item) => {
+    const cardElement = createCard(item.name, item.link, selectors.cardTemplate); 
+    cardList.setElement(cardElement);
+    }
+}, cardsSection);
+
+cardList.renderItems();
+
+const formValidators = {} //Объект для хранения валидаторов
+
+ //Универсальная функция для обработки валидатора форм
+ const enableValidation = (config) => {
+    const formList = Array.from(document.querySelectorAll(config.form));
+    formList.forEach((formElement) => {
+        const validator = new FormValidator(config, formElement);
+        const formName = formElement.getAttribute('name');
+
+        formValidators[formName] = validator;
+        validator.enableValidation();
+    });
+ };
+ 
+enableValidation(formSettings);
+
+popupOpenButtonEditProfile.addEventListener('click', () => {
+    formValidators.popupFormEditProfile.resetValidation();
+    fillPopupEdit();
+});
+
+popupOpenButtonAddCards.addEventListener('click', () => {
+    formCard.reset();
+    formValidators.popupFormAddCard.resetValidation();
+    createPopupExemplar(popupCards).open();
+});
