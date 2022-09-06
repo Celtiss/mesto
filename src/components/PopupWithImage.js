e976d160e4312bca42e94971d4d2f08b5b46f8d8
Elena Temirbekova <temn.lena@mail.ru>
2022-09-06 22:32:57 +0300

    fix: Сделать рефакторинг кода

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,11 +3,12 @@ import {Popup} from './Popup.js';
 export class PopupWithImage extends Popup {
     constructor (popupSelector) {
         super(popupSelector);
-        // вне конструктора this._popup = undefiend
-        // this._popup = document.querySelector('.popup_type_image'); 
+        //console.log(this._popup); // элемент: <div class="popup popup_type_image"></div>
+        // вне конструктора this._popup = undefined
     }
     
     open (name, link) {
+        //console.log(this._popup); // undefined
         this._popup = document.querySelector('.popup_type_image');
         this._popupImageName = this._popup.querySelector('.popup__image-heading');
         this._popupImageLink = this._popup.querySelector('.popup__image');
